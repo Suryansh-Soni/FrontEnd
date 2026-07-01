f4cc4162522779e2881ec92ff1526b6cf0390cf2
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-01 21:11:05 +0530

    working with props in react

--- react_start/src/App.jsx
+++ react_start/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Title from "./Title";
 import PdtTab from "./PdtTab";
+import MsgBox from "./MsgBox";
 function App() {
   return (
     <>
       <Title />
-      <PdtTab /></>
+      <PdtTab />
+      <MsgBox userName="John" testColor="blue" />
+      </>
     
   );
 }
