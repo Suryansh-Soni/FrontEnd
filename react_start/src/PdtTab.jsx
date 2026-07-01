b34edb9ab394f56686bf2c7637fa192b4f7c9472
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-01 19:56:38 +0530

    product tab component.

---
+++ react_start/src/PdtTab.jsx
@@ -0,0 +1,12 @@
+import Product from "./Product";
+function PdtTab() {
+  return (
+    <>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );
+}
+export default PdtTab;
