ce1427bd67ef3f006ff58112b01036ca796713ad
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-01 21:11:39 +0530

    different way to get props and handel them.

--- react_start/src/Product.jsx
+++ react_start/src/Product.jsx
@@ -1,8 +1,18 @@
-function Product() {
+function Product(props) {
+  const style = {
+    backgroundColor: props.price > 20 ? "lightcoral" : "lightgreen",
+    padding: "10px",
+    margin: "10px",
+    borderRadius: "5px",
+  };
+
   return (
-    <>
-      <p>pdt</p>
-    </>
+    <div style={style}>
+      <h2>{props.title}</h2>
+      <p>${props.price.toFixed(2)}</p>
+      <p>{props.description}</p>
+      {props.price > 20 && <p>This product is expensive</p>}
+    </div>
   );
 }
 
