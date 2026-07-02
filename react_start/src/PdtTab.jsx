e78298125cac75b57be41b5a8ac6a7222a730da6
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-02 21:12:31 +0530

    change in pdtTab

--- react_start/src/PdtTab.jsx
+++ react_start/src/PdtTab.jsx
@@ -2,26 +2,10 @@ import Product from "./Product";
 function PdtTab() {
   return (
     <>
-      <Product
-        title="Product 1"
-        price={19.99}
-        description="This is product 1"
-      />
-      <Product
-        title="Product 2"
-        price={29.99}
-        description="This is product 2"
-      />
-      <Product
-        title="Product 3"
-        price={39.99}
-        description="This is product 3"
-      />
-      <Product
-        title="Product 4"
-        price={49.99}
-        description="This is product 4"
-      />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
     </>
   );
 }
