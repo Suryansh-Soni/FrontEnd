5af8709fdcdcb868c9b9c510b6af96b04962ac9c
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-03 22:20:17 +0530

    like btn jsx

---
+++ react_state_class/src/LikeBtn.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function LikeBtn() {
+  const [isLiked, setIsLiked] = useState(false);
+
+  function toggleLike() {
+    setIsLiked(!isLiked);
+  }
+
+  return (
+    <div>
+      <h1>Like Button</h1>
+
+      <p onClick={toggleLike} style={{ cursor: "pointer", fontSize: "2rem" }}>
+        {isLiked ? (
+          <i className="fa-solid fa-heart" style={{ color: "red" }}></i>
+        ) : (
+          <i className="fa-regular fa-heart"></i>
+        )}
+      </p>
+    </div>
+  );
+}
