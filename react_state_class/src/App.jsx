ac795ba04ed0de6352251f5c89c13f7d5028fafc
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-03 22:20:04 +0530

    states example

---
+++ react_state_class/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LikeBtn from "./LikeBtn.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <LikeBtn />
+    </div>
+  );
+}
+
+export default App;
