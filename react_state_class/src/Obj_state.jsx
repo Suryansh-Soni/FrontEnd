989a837b2acfd7d698966e15d5088426391ddc50
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-05 05:16:02 +0530

    object state

--- react_state_class/src/Obj_state.jsx
+++ react_state_class/src/Obj_state.jsx
@@ -7,16 +7,27 @@ export default function State() {
       return [...prev, "hello world"];
     });
   }
-  function updateName() {
-    setObj({ ...obj, name: "Doe" });
+  // function updateName() {
+  //   setObj((prev) => {
+  //     return { ...prev, name: "Doe" };
+  //   });
+  // }
+
+  function updateName(){
+    setObj({...obj,name:"Doe"});
   }
   function updateAge() {
-    const age = obj.age + 1;
-    setObj({ ...obj, age: age });
+    setObj((prev)=>{
+      return {...prev,age:prev.age+1};
+    })
   }
+  // function updateAge() {
+  //   // const age = obj.age + 1;
+  //   setObj({ ...obj, age: obj.age + 1 });
+  // }
   return (
     <>
-      <h1 onClick={updateArr}>Array Elements:{arr}</h1>
+      <h1 onClick={updateArr}>Array Elements : {arr}</h1>
       <h1>the name is {obj.name}</h1>
       <button onClick={updateName}>Update Name</button>
       <p>the age is {obj.age}</p>
