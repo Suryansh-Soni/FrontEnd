14b1095db855ac7bc1788b592bda88affc2ca2e5
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-05 04:55:45 +0530

    change in app.jsx

--- react_state_class/src/App.jsx
+++ react_state_class/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import LikeBtn from "./LikeBtn.jsx";
-
+import Obj_state from "./Obj_state.jsx";
+import Todo from "./Todolist.jsx";
 function App() {
   return (
     <div className="App">
-      
-      <LikeBtn />
+      <Todo />
     </div>
   );
 }
