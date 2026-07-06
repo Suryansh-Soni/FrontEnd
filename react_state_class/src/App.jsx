db3595086a4c8334321ea7051e2dd8edb4cc1794
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-06 07:44:45 +0530

    updated app.jsx

--- react_state_class/src/App.jsx
+++ react_state_class/src/App.jsx
@@ -1,21 +1,7 @@
-import "./App.css";
-import LikeBtn from "./LikeBtn.jsx";
-import Obj_state from "./Obj_state.jsx";
-import Todo from "./Todolist.jsx";
-import Lottery from "./Lottery.jsx";
-import TicketNum from "./TicketNum.jsx";
-import Ticket from "./Ticket.jsx";
-import { sum } from "./helper.js";
-function App() {
-  let wincondition = (ticket) => {
-    sum(ticket) === 15;
-  };
+import Comment from "./commnet.jsx";
 
-  return (
-    <div className="App">
-      <Lottery n={3} wincondition={wincondition} />
-    </div>
-  );
+function App() {
+  return <Comment />;
 }
 
 export default App;
