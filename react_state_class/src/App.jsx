a6442b1e490fb2cfc365179e777a040627fac1b1
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-06 06:15:18 +0530

    .

--- react_state_class/src/App.jsx
+++ react_state_class/src/App.jsx
@@ -2,10 +2,18 @@ import "./App.css";
 import LikeBtn from "./LikeBtn.jsx";
 import Obj_state from "./Obj_state.jsx";
 import Todo from "./Todolist.jsx";
+import Lottery from "./Lottery.jsx";
+import TicketNum from "./TicketNum.jsx";
+import Ticket from "./Ticket.jsx";
+import { sum } from "./helper.js";
 function App() {
+  let wincondition = (ticket) => {
+    sum(ticket) === 15;
+  };
+
   return (
     <div className="App">
-      <Obj_state />
+      <Lottery n={3} wincondition={wincondition} />
     </div>
   );
 }
