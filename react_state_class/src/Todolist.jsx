ef5448dd13bc5746d062d3bb90f5fc829268e455
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-06 06:15:06 +0530

    todo list app

--- react_state_class/src/Todolist.jsx
+++ react_state_class/src/Todolist.jsx
@@ -2,18 +2,58 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  const [task, setTask] = useState([{ task: "any", id: uuidv4() }]);
+  const [task, setTask] = useState([
+    { task: "any", id: uuidv4(), isCompleted: false },
+  ]);
   const [newTodo, setNewTodo] = useState("");
 
   let addTask = () => {
     if (!newTodo.trim()) return;
 
-    setTask((prev) => [...prev, { task: newTodo, id: uuidv4() }]);
+    setTask((prev) => [
+      ...prev,
+      { task: newTodo, id: uuidv4(), isCompleted: false },
+    ]);
     setNewTodo("");
   };
 
   let updateTodo = (e) => {
     setNewTodo(e.target.value);
+  };
+
+  let deleteTask = (id) => {
+    setTask(() => task.filter((item) => item.id !== id));
+  };
+  let UpdatedAllTask = () => {
+    setTask(
+      task.map((prev) => {
+        return { ...prev, task: prev.task.toUpperCase() };
+      }),
+    );
+  };
+
+  let UpdateTask = (id) => {
+    setTask(
+      task.map((prev) => {
+        if (prev.id == id) {
+          return { ...prev, task: prev.task.toUpperCase() };
+        } else {
+          return prev;
+        }
+      }),
+    );
+  };
+
+  let toggleTaskCompletion = (id) => {
+    setTask(
+      task.map((prev) => {
+        if (prev.id == id) {
+          return { ...prev, isCompleted: !prev.isCompleted };
+        } else {
+          return prev;
+        }
+      }),
+    );
   };
 
   return (
@@ -23,10 +63,24 @@ export default function TodoList() {
       <button onClick={addTask}>Add Task</button>
 
       <ul>
-        {task.map((item) => (
-          <li key={item.id}>{item.task}</li>
+        {task.map((task) => (
+          <li
+            key={task.id}
+            style={{ textDecoration: task.isCompleted ? "line-through" : "" }}
+          >
+            <input
+              type="checkbox"
+              checked={task.isCompleted}
+              onChange={() => toggleTaskCompletion(task.id)}
+            />
+            {task.task}
+            &nbsp;&nbsp;
+            <button onClick={() => deleteTask(task.id)}>Delete task</button>
+            <button onClick={() => UpdateTask(task.id)}>Update task</button>
+          </li>
         ))}
       </ul>
+      <button onClick={UpdatedAllTask}>to Upper Case </button>
     </>
   );
 }
