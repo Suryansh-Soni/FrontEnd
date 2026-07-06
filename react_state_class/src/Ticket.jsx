579fcfc9630f82745ed5d7b4979c3d257f84501b
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-06 06:14:29 +0530

    ticket function following  lift state up method

---
+++ react_state_class/src/Ticket.jsx
@@ -0,0 +1,6 @@
+import TicketNum from "./TicketNum.jsx";
+export default function Ticket({ ticket }) {
+  return <div>{ticket.map((num, idx) => (
+    <TicketNum num={num} key={idx} />
+  ))} </div>;
+}
