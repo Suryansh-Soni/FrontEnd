a0152e6c044c358b58f2dd81d81dd3a8f9d45bd8
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-12 01:06:32 +0530

    app.jsx for todo

--- redux_class/src/App.jsx
+++ redux_class/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import { Provider } from "react-redux";
-import { store } from "../app/store.js";
-import Todo from "../components/todo.jsx";
+import { store } from "./app/store.js";
+import Todo from "./components/todo.jsx";
 
 function App() {
   return (
     <Provider store={store}>
-      <Todo />;
+      <Todo />
     </Provider>
   );
 }
+
 
 export default App;
